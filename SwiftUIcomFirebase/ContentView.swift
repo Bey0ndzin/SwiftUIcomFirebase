186cc32109d5053013d0d8d7b23491478b777d18
Pedro Nunes <zyxblackbeard@gmail.com>
2024-05-24 09:08:25 -0300

    Initial Commit

---
+++ SwiftUIcomFirebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIcomFirebase
+//
+//  Created by PEDRO HENRIQUE BATISTA NUNES  on 24/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIcomFirebase/SwiftUIcomFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIcomFirebaseApp.swift
+//  SwiftUIcomFirebase
+//
+//  Created by PEDRO HENRIQUE BATISTA NUNES  on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIcomFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
