7d70345e6bd9c36bf3ae717341dd6c9ad25f6b1f
Pedro Nunes <zyxblackbeard@gmail.com>
2024-05-28 11:33:45 -0300

    coiso o coiso

--- SwiftUIcomFirebase/Authentication/AuthenticationManager.swift
+++ SwiftUIcomFirebase/Authentication/AuthenticationManager.swift
@@ -6,11 +6,28 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+struct AuthDataResultModel {
+    
+    let uid: String
+    let email: String?
+    let photoUrl: String?
+    
+    init (user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoUrl = user.photoURL?.absoluteString
+    }
+}
 
 final class AuthenticationManager{
     
     static let shared = AuthenticationManager()
-    private init() {
-        
+    private init() { }
+    
+    func CreateUser(email: String, password: String) async throws -> AuthDataResultModel{
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
     }
 }
